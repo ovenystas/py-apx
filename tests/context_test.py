95edcbf74699a48942340559319365b7c206e658
cogu <congus8@gmail.com>
2018-07-10 15:57:50 +0200

    Added new unit test case

--- tests/context_test.py
+++ tests/context_test.py
@@ -8,10 +8,11 @@ import re
 
 
 class TestApxContext(unittest.TestCase):
-    
-   def test_dumps_from_raw(self):      
+
+
+   def test_dumps_from_raw(self):
       node = apx.Node('Simulator')
-      node.dataTypes.append(apx.DataType('InactiveActive_T','C(0,3)'))      
+      node.dataTypes.append(apx.DataType('InactiveActive_T','C(0,3)'))
       node.providePorts.append(apx.ProvidePort('VehicleSpeed','S','=65535'))
       node.providePorts.append(apx.ProvidePort('MainBeam','T[0]','=3'))
       node.providePorts.append(apx.ProvidePort('FuelLevel','C'))
@@ -30,8 +31,28 @@ class TestApxContext(unittest.TestCase):
       self.assertEqual(lines[5],'P"FuelLevel"C')
       self.assertEqual(lines[6],'P"ParkBrakeActive"T[0]:=3')
       self.assertEqual(lines[7],'R"RheostatLevelRqst"C:=255')
-      self.assertEqual(lines[8],'')         
+      self.assertEqual(lines[8],'')
+
+   def test_load_dumped_apx_text(self):
+      node1 = apx.Node('Simulator')
+      node1.dataTypes.append(apx.DataType('InactiveActive_T','C(0,3)'))
+      node1.providePorts.append(apx.ProvidePort('VehicleSpeed','S','=65535'))
+      node1.providePorts.append(apx.ProvidePort('MainBeam','T[0]','=3'))
+      node1.providePorts.append(apx.ProvidePort('FuelLevel','C'))
+      node1.providePorts.append(apx.ProvidePort('ParkBrakeActive','T[0]','=3'))
+      node1.requirePorts.append(apx.RequirePort('RheostatLevelRqst','C','=255'))
+      self.assertFalse(node1.providePorts[0].dsg.isComplexType())
+
+      context = apx.Context()
+      context.append(node1)
+      text=context.dumps()
+
+      apx_parser = apx.Parser()
+      node2 = apx_parser.loads(text)
+      self.assertFalse(node2.providePorts[0].dsg.isComplexType())
+
+
 
 if __name__ == '__main__':
-    unittest.main()   
+    unittest.main()
 
