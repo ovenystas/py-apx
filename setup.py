db2e4c07cc2a987685920ba9c781e32be74f64a4
cogu <congus8@gmail.com>
2018-11-30 12:21:50 +0100

    Updated internal version to 0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def test_suite():
     return suite		
 
 setup(name='apx',
-      version='0.3.0',
+      version='0.3.1',
       description='Official APX python toolchain and client',
       long_description=readme(),
       classifiers=[
